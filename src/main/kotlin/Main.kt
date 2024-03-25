b36627c0e0941b8fd031cf0faa58ea29b88e133b
amukoaterry <amukoaterrymitchell@gmail>
2024-03-25 08:36:00 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+   var words=sentence("Barnie bakes brown bangles and buns")
+
+}
+
+fun sentence(fact:String){
+    println(fact)
+
+    println(numbers(32,46))
+}
+
+fun numbers(nums:Int,numz:Int){
+
+
+
+
+
+}
+
+
+
+
+
+
